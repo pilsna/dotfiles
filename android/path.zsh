a2b02965829515218800e166f2c87e53ce134dd0
Nils Rydh <nils.rydh@gmail.com>
2014-05-30 11:05:02 +0200

    added eclipse to the path

--- android/path.zsh
+++ android/path.zsh
@@ -1,2 +1,4 @@
+# the latest google adt-bundle is placed in ~/Library/Developer and manually symlinked to ~/adt
 export PATH=$PATH:~/adt/sdk/platform-tools
 export PATH=$PATH:~/adt/sdk/tools
+export PATH=$PATH:~/adt/eclipse/Eclipse.app/Contents/MacOS
