890c85c67808c4779802ee64624ab17711b60cb5
Nils Rydh <nils@risedigital.dk>
2018-12-18 21:52:08 +0100

    removed sublime stuff, updated some stuff

--- nodejs/completion.zsh
+++ nodejs/completion.zsh
@@ -1,1 +1,5 @@
 eval "$(npm completion 2>/dev/null)"
+# nvm 
+export NVM_DIR="/Users/nils/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- nodejs/path.zsh
+++ nodejs/path.zsh
@@ -1,2 +1,3 @@
 # to make sure applications can include npm and other global stuff...
-export NODE_PATH=/usr/local/lib/node_modules
+# export NODE_PATH=/usr/local/lib/node_modules
+
