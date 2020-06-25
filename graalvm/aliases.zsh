13fab7187c01b9f8a0f7f866a807e5f33ff54315
Nils Rydh <ngr@verticalstrategy.com>
2020-06-25 23:17:28 +0200

    cleaned up python and graalvm

--- graalvm/aliases.zsh
+++
@@ -1,1 +0,0 @@
-alias gu='/Users/nils/java/graalvm-ce-java11-19.3.0/Contents/Home/bin/gu'
---
+++ python/path.zsh
@@ -0,0 +1,5 @@
+export VIRTUALENVWRAPPER_LOG_FILE=.virtualenvwrapper.log
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+export CURL_CA_BUNDLE=
+source /usr/local/bin/virtualenvwrapper.sh
