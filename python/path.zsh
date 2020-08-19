74e80ddf866610260bcf75ba12c79652875a24a4
pilsna <nils.rydh@gmail.com>
2020-08-19 14:59:35 +0200

    install on new bain mac

--- python/path.zsh
+++ python/path.zsh
@@ -2,4 +2,5 @@ export VIRTUALENVWRAPPER_LOG_FILE=.virtualenvwrapper.log
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
 export CURL_CA_BUNDLE=
-source /usr/local/bin/virtualenvwrapper.sh
+# source /usr/local/bin/virtualenvwrapper.sh
+eval "$(pyenv virtualenv-init -)"
