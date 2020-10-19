99d6ab62f28649021ddebac68f4033ad5f60297f
Nils Rydh <nils.rydh@gmail.com>
2020-10-19 22:58:03 +0200

    updated python env setup

--- python/path.zsh
+++ python/path.zsh
@@ -1,6 +1,6 @@
-export VIRTUALENVWRAPPER_LOG_FILE=.virtualenvwrapper.log
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-export CURL_CA_BUNDLE=
+## export VIRTUALENVWRAPPER_LOG_FILE=.virtualenvwrapper.log
+## export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+## export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+## export CURL_CA_BUNDLE=
 # source /usr/local/bin/virtualenvwrapper.sh
-eval "$(pyenv virtualenv-init -)"
+## eval "$(pyenv virtualenv-init -)"
