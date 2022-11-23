6ab65e9b73320187add7269ed89fbd66ab113c39
Nils Rydh <nils.rydh@bain.com>
2022-11-23 16:13:18 +0100

    Updated graalvm path to be user agnostic. Changed nvm to load on first use. Disable pyenv and added python3 version to path. Added qmk config for compiling black pill. Cleaned up the .zshrc.

--- graalvm/path.zsh
+++ graalvm/path.zsh
@@ -1,3 +1,3 @@
-export GRAALVM_HOME=/Users/nils/java/graalvm-ce-java11-20.1.0
+export GRAALVM_HOME=$HOME/java/graalvm-ce-java11-20.1.0
 export PATH=$PATH:$GRAALVM_HOME/Contents/Home/bin
 
---
+++ qmk/path.zsh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/opt/arm-none-eabi-gcc@8/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/arm-none-eabi-gcc@8/lib"
+
+export PATH="/usr/local/opt/avr-gcc@8/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/avr-gcc@8/lib"
