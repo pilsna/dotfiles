7c6169ce7a8f153aa131cb1b231616f918dd7ff9
Nils Rydh <nils.rydh@bain.com>
2022-12-29 21:10:28 +0100

    simplified zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,4 +4,4 @@ precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats '%b '
 
 setopt PROMPT_SUBST
-PROMPT='%F{green}%*%f %F{blue}%~%f %F{red}${vcs_info_msg_0_}%f# '
+PROMPT='%F{green}%*%f %F{blue}%~%f %F{red}${vcs_info_msg_0_}%f%# '
