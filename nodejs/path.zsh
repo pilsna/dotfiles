78f81ba94da40143f4348085d3f5d2e87b03ac7f
Nils Rydh <nils.rydh@bain.com>
2023-12-26 20:29:54 +0100

    update to node and iterm

--- nodejs/path.zsh
+++ nodejs/path.zsh
@@ -1,3 +1,4 @@
 # to make sure applications can include npm and other global stuff...
 # export NODE_PATH=/usr/local/lib/node_modules
-
+ export NODE_EXTRA_CA_CERTS=/Users/51048/Documents/Zscaler\ Root\ CA.pem
+ 
